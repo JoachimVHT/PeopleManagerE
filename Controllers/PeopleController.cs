4792ba2881575a78f4e8a078eb8d3ab78d93b0e5
Joachim Van haecht <joachim.vanhaecht@student.vives.be>
2021-04-09 20:37:45 +0200

    edit en delete pagina voor persoon evenals delete functie met modals

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -26,5 +26,97 @@ namespace PeopleManagerEigen.Controllers
         {
             return _database.People;
         }
+
+        
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Persoon persoon)
+        {
+            persoon.Id = GetNewId();
+            
+            _database.People.Add(persoon);
+
+            return RedirectToAction("Index");
+        }
+
+        private int GetNewId()
+        {
+            if (_database.People.Any())
+            {
+                var getMaxId = _database.People.Max(p => p.Id);
+                return getMaxId += 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var gezochtePers = _database.People.SingleOrDefault(p => p.Id == id);
+            if(gezochtePers == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(gezochtePers);
+            }
+
+            
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Persoon persoon)
+        {
+            var pers = _database.People.SingleOrDefault(p => p.Id == persoon.Id);
+
+            if(pers != null)
+            {
+                pers.Email = persoon.Email;
+                pers.Familienaam = persoon.Familienaam;
+                pers.Voornaam = persoon.Voornaam;
+            }
+           
+            return RedirectToAction("Index");
+
+        }
+        
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var pers = _database.People.SingleOrDefault(p => p.Id == id);
+            if (pers != null)
+            {
+                return View(pers);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Persoon persoon)
+        {
+            var pers = _database.People.SingleOrDefault(p => p.Id == persoon.Id);
+            if(pers != null)
+            {
+                _database.People.Remove(pers);
+            }
+            return RedirectToAction("Index");
+
+        }
     }
 }
